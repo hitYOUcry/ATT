30e3417d303b204f420953d0ea3ce91d6bf69779
hitYOUcry <285692817@qq.com>
2015-07-17 20:03:17 +0800

    almost finish

--- src/com/example/att/activity/MainActivity.java
+++ src/com/example/att/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.att.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.att.R;
+import com.example.att.view.LineView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private LineView mLineView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +18,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void initView() {
-
+        mLineView = (LineView) findViewById(R.id.linegraph);
+        mLineView.addLine(45f);
+        mLineView.addLine(150f);
+        mLineView.addLine(250f);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
--- src/com/example/att/view/LineView.java
+++ src/com/example/att/view/LineView.java
@@ -1,22 +1,32 @@
 package com.example.att.view;
 
-import com.example.att.R;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Typeface;
+import android.graphics.Paint.Align;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.example.att.R;
+
 public class LineView extends View {
 
+    private final static int NUMS = 3;
+    private final static int offset = 30;
+    private final static float radius = 5.0f;
+
     private int mWidth;
+
     private int mTextSize;
     private int mTextColor;
-    private CharSequence mText;
+
+    private float[] xAixs = new float[NUMS];
+    private List<Float> yAxisList;
     private TextPaint mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
     private Paint mLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
@@ -25,18 +35,57 @@ public class LineView extends View {
     }
 
     public LineView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public LineView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        TypedArray attributes = context.obtainStyledAttributes(attrs,R.styleable.LineGraph);
-        mTextSize = attributes.getDimensionPixelSize(R.styleable.LineGraph_text_size, defValue)
+        TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.LineGraph);
+        mTextSize =
+                attributes.getDimensionPixelSize(R.styleable.LineGraph_text_size, getResources()
+                        .getDimensionPixelSize(R.dimen.linegraph_text_size));
+        mTextColor =
+                attributes.getColor(R.styleable.LineGraph_text_color,
+                        getResources().getColor(R.color.linegraph_text_color));
+        yAxisList = new ArrayList<Float>();
+
+        attributes.recycle();
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        initData();
+        mTextPaint.setColor(mTextColor);
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setTextAlign(Align.CENTER);
+        int y = getBottom() - offset;
+        canvas.drawLine(getLeft() + offset, y, getRight() - offset, y, mLinePaint);
+        for (int i = 0; i < yAxisList.size(); i++) {
+            canvas.drawLine(xAixs[i], y, xAixs[i], y - yAxisList.get(i), mLinePaint);
+            canvas.drawCircle(xAixs[i], y - yAxisList.get(i), radius, mLinePaint);
+            canvas.drawText(String.format("%1$.2fdB", yAxisList.get(i)), xAixs[i],
+                    y - yAxisList.get(i) - 3 * radius, mTextPaint);
+        }
+
+    }
+
+    public void addLine(float height) {
+        if (null != yAxisList) {
+            if (yAxisList.size() >= NUMS) {
+                yAxisList.remove(0); // remove first;
+            }
+            yAxisList.add(height);
+            invalidate();
+        }
+    }
+
+    private void initData() {
+        mWidth = getWidth();
+        for (int i = 0; i < xAixs.length; i++) {
+            xAixs[i] = 2 * offset + i * (float) mWidth / NUMS;
+        }
     }
 
 
